a8abe043480a35852d340c6f7466b20ac25f5845
prakashversion1 <prakashjoshiversion1@gmail.com>
2014-12-30 20:58:38 +0545

    Changed code according to original example now stable as nonRx implementation

--- src/main/java/com/mycompany/myproject/PingVerticle.java
+++ src/main/java/com/mycompany/myproject/PingVerticle.java
@@ -22,149 +22,166 @@ import org.vertx.java.core.Handler;
 import org.vertx.java.core.MultiMap;
 import org.vertx.java.core.buffer.Buffer;
 import org.vertx.java.core.eventbus.Message;
+import org.vertx.java.core.http.HttpServer;
 import org.vertx.java.core.http.HttpServerRequest;
 import org.vertx.java.core.http.RouteMatcher;
 import org.vertx.java.core.json.JsonArray;
 import org.vertx.java.core.json.JsonObject;
-import org.vertx.java.core.logging.Logger;
 import org.vertx.java.platform.Verticle;
 
-/*
- * This is a simple Java verticle which receives `ping` messages on the event bus and sends back `pong` replies
- *
- * @author <a href="http://tfox.org">Tim Fox</a>
- */
 public class PingVerticle extends Verticle {
 
-  public void start() {
+    public void start() {
 
-    JsonObject appConfig = container.config();
+        // load the general config object, loaded by using -config on command line
+        JsonObject appConfig = container.config();
 
-    System.out.println("Running the ping Vertice");
+        // deploy the mongo-persistor module, which we'll use for persistence
+        container.deployModule("io.vertx~mod-mongo-persistor~2.1.0", appConfig.getObject("mongo-persistor"));
 
-    container.deployModule("io.vertx~mod-mongo-persistor~2.0.0-beta2", appConfig.getObject("mongo-persistor"));
+        System.out.println("Successfully deployed modngo module");
 
-    vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
-      @Override
-      public void handle(final HttpServerRequest httpServerRequest) {
+        // setup Routematcher
+        RouteMatcher matcher = new RouteMatcher();
 
-        RouteMatcher routeMatcher = new RouteMatcher();
-        routeMatcher.get("/zips", new Handler<HttpServerRequest>() {
-          @Override
-          public void handle(HttpServerRequest request) {
-            JsonObject json = new JsonObject();
-            MultiMap params = request.params();
-            if (params.size() > 0 && params.contains("state") || params.contains("city")) {
-              // create the matcher configuration
-              JsonObject matcher = new JsonObject();
-              if (params.contains("state")) matcher.putString("state", params.get("state"));
-              if (params.contains("city")) matcher.putString("city", params.get("city"));
+        // the matcher for the complete list and the search
+        matcher.get("/zips", new Handler<HttpServerRequest>() {
+            public void handle(final HttpServerRequest req) {
 
-              // create the message for the mongo-persistor verticle
-              json = new JsonObject().putString("collection", "zips")
-                      .putString("action", "find")
-                      .putObject("matcher", matcher);
+                JsonObject json = new JsonObject();
+                MultiMap params = req.params();
 
-            } else {
-              // create the query
-              json = new JsonObject().putString("collection", "zips")
-                      .putString("action", "find")
-                      .putObject("matcher", new JsonObject());
+                if (params.size() > 0 && params.contains("state") || params.contains("city")) {
+                    // create the matcher configuration
+                    JsonObject matcher = new JsonObject();
+                    if (params.contains("state")) matcher.putString("state", params.get("state"));
+                    if (params.contains("city")) matcher.putString("city", params.get("city"));
+
+                    // create the message for the mongo-persistor verticle
+                    json = new JsonObject().putString("collection", "zips")
+                            .putString("action", "find")
+                            .putObject("matcher", matcher);
+
+                } else {
+                    // create the query
+                    json = new JsonObject().putString("collection", "zips")
+                            .putString("action", "find")
+                            .putObject("matcher", new JsonObject());
+                }
+
+                JsonObject data = new JsonObject();
+                data.putArray("results", new JsonArray());
+                // and call the event we want to use
+                vertx.eventBus().send("mongodb-persistor", json, new ReplyHandler(req, data));
             }
-
-            JsonObject data = new JsonObject();
-            data.putArray("results", new JsonArray());
-            // and call the event we want to use
-            vertx.eventBus().send("mongodb-persistor", json, new ReplyHandler(request, data));
-          }
         });
 
-        routeMatcher.post("/zips/:id", new Handler<HttpServerRequest>() {
-
-          public void handle(final HttpServerRequest req) {
-            // process the body
-            req.bodyHandler(new Handler<Buffer>() {
-
-              @Override
-              public void handle(Buffer event) {
-                // normally we'd validate the input, for now just assume it is correct.
-                final String body = event.getString(0,event.length());
+        // the matcher for a specific id
+        matcher.get("/zips/:id", new Handler<HttpServerRequest>() {
+            public void handle(final HttpServerRequest req) {
+                String idToRetrieve = req.params().get("id");
 
                 // create the query
-                JsonObject newObject = new JsonObject(body);
-                JsonObject matcher = new JsonObject().putString("_id", req.params().get("id"));
+                JsonObject matcher = new JsonObject().putString("_id", idToRetrieve);
                 JsonObject json = new JsonObject().putString("collection", "zips")
-                        .putString("action", "update")
-                        .putObject("criteria", matcher)
-                        .putBoolean("upsert", false)
-                        .putBoolean("multi",false)
-                        .putObject("objNew",newObject);
+                        .putString("action", "find")
+                        .putObject("matcher", matcher);
 
                 // and call the event we want to use
                 vertx.eventBus().send("mongodb-persistor", json, new Handler<Message<JsonObject>>() {
-                  @Override
-                  public void handle(Message<JsonObject> event) {
-                    // we could handle the errors here, but for now
-                    // assume everything went ok, and return the original
-                    // and updated json
-                    req.response().end(body);
-                  }
+                    @Override
+                    public void handle(Message<JsonObject> event) {
+                        req.response().putHeader("Content-Type", "application/json");
+                        if (event.body().getArray("results").size() > 0) {
+                            JsonObject result = event.body().getArray("results").get(0);
+                            req.response().end(result.encodePrettily());
+                        }
+                    }
                 });
-              }
-            });
-          }
+            }
+        });
+
+        // the matcher for the update
+        matcher.post("/zips/:id", new Handler<HttpServerRequest>() {
+            public void handle(final HttpServerRequest req) {
+
+                // process the body
+                req.bodyHandler(new Handler<Buffer>() {
+
+                    @Override
+                    public void handle(Buffer event) {
+                        // normally we'd validate the input, for now just assume it is correct.
+                        final String body = event.getString(0, event.length());
+
+                        // create the query
+                        JsonObject newObject = new JsonObject(body);
+                        JsonObject matcher = new JsonObject().putString("_id", req.params().get("id"));
+                        JsonObject json = new JsonObject().putString("collection", "zips")
+                                .putString("action", "update")
+                                .putObject("criteria", matcher)
+                                .putBoolean("upsert", false)
+                                .putBoolean("multi", false)
+                                .putObject("objNew", newObject);
+
+                        // and call the event we want to use
+                        vertx.eventBus().send("mongodb-persistor", json, new Handler<Message<JsonObject>>() {
+                            @Override
+                            public void handle(Message<JsonObject> event) {
+                                // we could handle the errors here, but for now
+                                // assume everything went ok, and return the original
+                                // and updated json
+                                req.response().end(body);
+                            }
+                        });
+                    }
+                });
+            }
         });
 
 
-        routeMatcher.get("/zips[id]", new Handler<HttpServerRequest>() {
-          @Override
-          public void handle(HttpServerRequest request) {
+        // create and run the server
+        HttpServer server = vertx.createHttpServer().requestHandler(matcher).listen(8080);
 
-          }
-        });
-
-      }
-    }).listen(8080);
-
-    // output that the server is started
-    container.logger().info("Webserver started, listening on port: 8080");
-  }
-
-
-
-
-  private class ReplyHandler implements Handler<Message<JsonObject>> {
-
-    private final HttpServerRequest request;
-    private JsonObject data;
-
-    private ReplyHandler(final HttpServerRequest request, JsonObject data) {
-      this.request = request;
-      this.data = data;
+        // output that the server is started
+        System.out.println("Webserver started, listening on port: 8080");
     }
 
-    @Override
-    public void handle(Message<JsonObject> event) {
-      // if the response contains more message, we need to get the rest
-      if (event.body().getString("status").equals("more-exist")) {
-        JsonArray results = event.body().getArray("results");
 
-        for (Object el : results) {
-          data.getArray("results").add(el);
+    /**
+     * Simple handler that can be used to handle the reply from mongodb-persistor
+     * and handles the 'more-exist' field.
+     */
+    private static class ReplyHandler implements Handler<Message<JsonObject>> {
+
+        private final HttpServerRequest request;
+        private JsonObject data;
+
+        private ReplyHandler(final HttpServerRequest request, JsonObject data) {
+            this.request = request;
+            this.data = data;
         }
 
-        event.reply(new JsonObject(), new ReplyHandler(request, data));
-      } else {
+        @Override
+        public void handle(Message<JsonObject> event) {
+            // if the response contains more message, we need to get the rest
+            if (event.body().getString("status").equals("more-exist")) {
+                JsonArray results = event.body().getArray("results");
 
-        JsonArray results = event.body().getArray("results");
-        for (Object el : results) {
-          data.getArray("results").add(el);
+                for (Object el : results) {
+                    data.getArray("results").add(el);
+                }
+
+                event.reply(new JsonObject(), new ReplyHandler(request, data));
+            } else {
+
+                JsonArray results = event.body().getArray("results");
+                for (Object el : results) {
+                    data.getArray("results").add(el);
+                }
+
+                request.response().putHeader("Content-Type", "application/json");
+                request.response().end(data.encodePrettily());
+            }
         }
-
-        request.response().putHeader("Content-Type", "application/json");
-        request.response().end(data.encodePrettily());
-      }
     }
-  }
 }
-
